57b7489fca4081fb0b7ca58a1ac019e9a0dc001e
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-09-19 11:21:12 -0400

    completes through step #33

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import TrackList from '../TrackList/TrackList';
 import './SearchResults.css';
 
 class SearchResults extends React.Component {
+   // constructor(props) {
+   //    super(props);
+   // }
    render(){
       return (
          <div className="SearchResults">
             <h2>Results</h2>
             {/* <!-- Add a TrackList component --> */}
+            <TrackList 
+               tracks={this.props.SearchResults}
+            />
          </div>
       )
    }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,7 +6,7 @@ import "./TrackList.css";
 class TrackList extends React.Component {
    render(){
       return (
-         <div class="TrackList">
+         <div className="TrackList">
             {/* <!-- You will add a map method that renders a set of Track components  --> */}
          </div>
       )
