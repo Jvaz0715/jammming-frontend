99480feb79b86b571f2ea9f89d2ee70139185654
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-09-19 10:54:59 -0400

    Jammming Codecademy project steps #01-28 completed. Refactors all components to use React.Component.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,28 @@
-import './App.css';
+import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import PlayList from '../PlayList/PlayList';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-      {/* <!-- Add a SearchBar component --> */}
-      <SearchBar />
-        <div className="App-playlist">
-        {/* <!-- Add a SearchResults component --> */}
-        <SearchResults />
-        {/* <!-- Add a Playlist component --> */}
-        <PlayList />
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* <!-- Add a SearchBar component --> */}
+          <SearchBar />
+          <div className="App-playlist">
+            {/* <!-- Add a SearchResults component --> */}
+            <SearchResults />
+            {/* <!-- Add a Playlist component --> */}
+            <PlayList />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+};
 
 export default App;
